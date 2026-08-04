90673e74745ec214292987a2c68c9460f7bb5f2f
Kitties <nemyrexe@gmail.com>
2026-08-04 16:07:25 +0700

    added loading screen

---
+++ Assets/_Game/Code/UI/LoadingScreen.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Game.Code.UI
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        [SerializeField] private Image _loadingIcon;
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private float _loadingIconRotationSpeed = 15;
+        
+        private Coroutine _fadeOutCoroutine;
+        private Coroutine _rotateIconCoroutine;
+
+        public void Show()
+        {
+            StopFadeOut();
+            _canvasGroup.alpha = 1;
+            _rotateIconCoroutine = StartCoroutine(RotateIcon());
+        }
+
+        public void Hide()
+        {
+            StopFadeOut();
+            _fadeOutCoroutine = StartCoroutine(FadeOut());
+        }
+
+        private void StopFadeOut()
+        {
+            if(_fadeOutCoroutine != null)
+                StopCoroutine(_fadeOutCoroutine);
+        }
+
+        private IEnumerator FadeOut()
+        {
+            while (_canvasGroup.alpha > 0)
+            {
+                _canvasGroup.alpha -= 0.01f;
+                yield return null;
+            }
+            
+            _canvasGroup.alpha = 0;
+            StopRotateIcon();
+        }
+
+        private void StopRotateIcon()
+        {
+            if(_rotateIconCoroutine != null)
+                StopCoroutine(_rotateIconCoroutine);
+        }
+
+        private IEnumerator RotateIcon()
+        {
+            while (true)
+            {
+                _loadingIcon.transform.Rotate(0, 0, _loadingIconRotationSpeed);
+                yield return null;
+            }
+        }
+    }
+}
